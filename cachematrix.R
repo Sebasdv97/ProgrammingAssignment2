fce3af1b9e8ec17a4ea1dfb9791a32ab00ffc4a0
Sebasdv97 <62575146+Sebasdv97@users.noreply.github.com>
2020-03-30 23:50:17 -0300

    Update cachematrix.R
    completes comments in code

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,26 @@
 makeCacheMatrix <- function(x = matrix()) {
   inv<-NULL
-  set <- function(y) {
-    x <<- y #save x in othr enviroment
+  set <- function(y) {#load a matrix into 'makeCacheMatrix'
+    x <<- y #save x in other enviroment
     inv<<- NULL #reset inv in other enviroment
   } 
   get <- function() x #get the matrix
-  setinv <- function(inverse) inv <<- inverse#set marix sobre la que quiero la inversa
+  setinv <- function(inverse) inv <<- inverse#set marix from what I want the inverse
   getinv<- function() inv #get matrix inv
   list(set = set, get = get,
           setinv = setinv,
-          getinv = getinv) #create the list
+          getinv = getinv) #create the list with matrix and inverse
   
 }
 
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) { #return the inverse matrix from the cache  or solve a matrix and then store the inverse matrix in the cache.
   inv <- x$getinv() #search the inv in cache memory
   if(!is.null(inv)) { #compare if the inv is in the cache
     message("getting cached data")
     return(inv)
   }
-  data <- x$get() #saves the marix to be inverted in data
+  data <- x$get() #saves the marix to be inverted in data (temporal memory)
   inv <- solve(data, ...) #calcultes the inverted matrix
   x$setinv(inv) # Saves the inverted matrix in the cache
   inv #shows result
-}
